3245943407e593004d418be602d76edecfcdeeca
Simranjit911 <simranjit94938@gmail.com>
2023-06-10 20:41:30 +0530

    first commit

---
+++ 1-js-intro/index.js
@@ -0,0 +1,16 @@
+console.log('hi')
+console.log(5+5)
+let a=25.5
+console.log(a)
+var mydata={
+    name:"sam",
+    age:21
+}
+console.log(mydata)
+let cars=['G-wagon','Urus','Lc','volvo','range']
+console.log(cars)
+cars.push('Rolls Royace')
+cars.unshift('Rolls Royace')
+console.log(cars)
+cars.shift()
+console.log(cars.length)
