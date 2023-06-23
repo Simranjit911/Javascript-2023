bb6a6521a3d25363ddad232c3e27ecacfd5d945a
Simranjit911 <simranjit94938@gmail.com>
2023-06-23 21:40:37 +0530

    idk

---
+++ Practice set/7.Practicer set/app.js
@@ -0,0 +1,6 @@
+let itm1=document.querySelector('.itm1').style.color='red'
+let itm3=document.querySelector('.itm3').style.color='cyan'
+let itm2=document.querySelector('.itm2').style.color='cyan'
+Array.from(document.getElementsByTagName('ul')).forEach((el)=>{
+    el.style.background='black'
+})
